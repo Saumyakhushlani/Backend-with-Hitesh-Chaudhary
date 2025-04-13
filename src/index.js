79fc5eb26ff2ced07b4a6072500b17328dcc2cfc
Saumya khushlani <saumyakhushlani9@gmail.com>
2025-04-14 00:32:19 +0530

    Database Connected

--- src/index.js
+++ src/index.js
@@ -2,20 +2,22 @@
 
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
+import {app} from './app.js'
 
 dotenv.config({
     path:'./env'
 })
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(`Server is running on port ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log(`Error in Database Connection`,err);
+})
 
 
 
 
-// ( async()=>{
-//     try {
-//       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`) 
-//     } catch (error) {
-//         console.log("Error: ",error);
-//     }
-// })()
