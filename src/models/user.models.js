5408d6e949abdffd62c2170740e2772e854a998f
Saumya khushlani <saumyakhushlani9@gmail.com>
2025-06-28 00:44:20 +0530

    Concept of meathords and pre and post hooks

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 const userSchema = new Schema(
     {
@@ -47,5 +49,39 @@ const userSchema = new Schema(
     },
     { timestamps: true }
 )
+
+userSchema.pre("save", async function (next){
+
+    if(!this.modified("password")) return next()
+    this.password=bcrypt.hash(this.password,10)
+
+    next()
+})
+
+userSchema.methods.isPasswordCorrect= async function (password){
+   return await bcrypt.compare(password,this.password)
+}
+
+userSchema.methods.genrateAcesstoken =async function(){
+    return jwt.sign({
+        _id:this._id,
+        email:this.email,
+        username:this.username
+    },
+    process.env.TOKEN_SECRET,
+    {expiresIn:"1d"}
+)
+}
+
+
+userSchema.methods.genrateRefreshtoken =async function(){
+    return jwt.sign({
+        _id:this._id,
+        
+    },
+    process.env.TOKEN_SECRET,
+    {expiresIn:"1d"}
+)
+}
 
 export const User = mongoose.model("User", userSchema)
